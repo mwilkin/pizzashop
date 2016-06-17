99f6ab0fada9df2312c7928d5230436a774f9ba9
matt wilkin <mwilkin22@gmail.com>
2016-06-17 15:57:54 -0700

    fix errors in Javascript and html reference errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-//business logic
+//Business Logic:
 var customer1 ="";
 
-function Customer(customername, size, crust, sauce, cheese, veg){
-  this.customername;
-  this.customerphone;
+function Customer(customername, size, crust, sauce, cheese, veg) {
+  this.customername="";
+  this.customerphone="";
   this.pizzanumber = 0;
   this.pizzasize = size;
   this.pizzacrust = crust;
@@ -14,58 +14,107 @@ function Customer(customername, size, crust, sauce, cheese, veg){
   this.total = 0;
 }
 
-Customer.prototype.cost = function (){
-   return [this.pizzanumber *(this.pizzasize + this.pizzacrust + this.pizzasauce + this.pizzacheese + this.pizzameat + this.pizzaveg)]
-};
+Customer.prototype.size = function() {
+  if (this.pizzasize === "Small") {
+    return this.total += 10;
+  } else if (this.pizzasize === "Medium") {
+    return this.total += 12;
+  } else if (this.pizzasize === "Large") {
+    return this.total += 15;
+  } else {
+    return this.total += 18;
+  }
+}
 
-var clearValues = function (){
+Customer.prototype.crust = function() {
+  if (this.pizzacrust === "Traditional") {
+    return this.total += 1;
+  } else if (this.pizzacrust === "Gluten Free") {
+    return this.total += 3;
+  } else {
+    return this.total +=5;
+  }
+}
+
+Customer.prototype.sauce = function() {
+  if (this.pizzasauce === "Tomato") {
+  return this.total +=1;
+  } else {
+  return this.total +=3;
+  }
+}
+
+Customer.prototype.cheese = function() {
+  if (this.pizzasauce === "Fresh Mozzarella") {
+  return this.total +=5;
+  } else if (this.pizzasauce === "Ricotta") {
+    return this.total +=3;
+  } else {
+    return this.total +=1;
+  }
+}
+
+Customer.prototype.meat = function() {
+  var meatNumber = this.pizzameat.length;
+}
+
+Customer.prototype.veg = function() {
+  var vegNumber = this.pizzaveg.length;
+}
+
+Customer.prototype.cost = function() {
+  return this.total = [this.pizzanumber * this.total]
+}
+
+// User Interface functions:
+var clearValues = function(){
   $(".customerName").val("");
   $(".customerPhone").val("");
+  $("#pizza-number").val("");
+  $("#pizza-size").val("");
+  $("#pizza-crust").val("");
+  $("#pizza-sauce").val("");
+  $("#cheese").val("");
 }
 
-var checkTally = function (){
-
+var displaySelection = function() {
+  $("#orderForm").hide();
+  $(".orderSummary").show();
+  $(".customerName").text(customer1.customerName);
+  $(".customerPhone").text(customer1.customerPhone);
+  $(".pizza-size").text(customer1.pizzasize);
+  $(".pizza-crust").text(customer1.pizzacrust);
+  $(".pizza-sauce").text(customer1.pizzasauce);
+  $(".pizza-cheese").text(customer1.pizzacheese);
+  $(".pizza-meat").text(customer1.pizzameat);
+  $(".pizza-veggies").text(customer1.pizzaveg);
+  $(".pizza-price").text(customer1.total);
 }
 
-
-//user Interface
+//User Interface:
 $(document).ready(function(){
-  $("#reviewOrder").click(function(even){
+  $("#reviewOrder").click(function(event){
     event.preventDefault();
     var customerName = $(".customerName").val();
     var customerPhone = $(".customerPhone").val();
-
     var number = $("#pizza-number").val();
     var size = $("#pizza-size").val();
     var crust = $("#pizza-crust").val();
     var sauces = $("#pizza-sauce").val();
     var cheese = $("#cheese").val();
+    // debugger;
     var meat = [];
-    $("input:checkbox[name='meat']:checked").each(function() {
+    var vegetable = [];
+    $("input:checkbox[name='meat']:checked").each(function(){
       (meat).push(this.value);
     });
-    var vegetable = [];
-    $("input:checkbox[name='vegetable']:checked").each(function() {
+    $("input:checkbox[name='vegetable']:checked").each(function(){
       (vegetable).push(this.value);
     });
-    $("#orderForm").hide();
-    $(".orderSummary").show();
-    $(".customerName").text(customerName);
-    $(".customerPhone").text(customerPhone);
-    $(".pizza-size").value(size);
-    $(".pizza-crust").text(crust);
-    $(".pizza-sauce").text(sauce);
-    $(".pizza-cheese").text(cheese);
-    $(".pizza-meat").text(meat);
-    $(".pizza-veggies").text(vegetable);
-
-
-
+    displaySelection();
 
   });
-  $("#placeOrder").click(function(event){
-
-    $(".pizza-meat").text(meat);
+  $("#placeOrder").click(function(event) {
     $(".orderSummary").hide();
     $(".orderSubmitted").show();
     clearValues();
